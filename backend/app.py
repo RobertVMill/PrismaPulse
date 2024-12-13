773ad851a964b99083927a073e099b7e6c002517
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2024-12-12 20:32:24 -0500

    feat: update CORS to allow Vercel domain

--- backend/app.py
+++ backend/app.py
@@ -33,7 +33,7 @@ app = Flask(__name__)
 CORS(app, 
      resources={
          r"/*": {
-             "origins": ["http://localhost:3000"],
+             "origins": ["http://localhost:3000", "https://prisma-pulse.vercel.app"],
              "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
              "allow_headers": ["Content-Type", "Authorization"],
              "supports_credentials": True
